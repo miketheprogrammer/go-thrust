c84d927e324fe171c95db9ccf5d2cecfa3ebcd5b
Michael Hernandez <michael.hernandez1988@gmail.com>
2014-10-17 16:26:23 -0400

    Fixed tools

--- release/tools/bootstrap_darwin.sh
+++ release/tools/bootstrap_darwin.sh
@@ -1,6 +1,5 @@
-rm -rf thrust-v*
-wget https://github.com/breach/thrust/releases/download/v0.7.0/thrust-v0.7.0-linux-x64.zip
-mv thrust-v0.7.0-linux-x64.zip ./vendor/linux/x64
-cd ./vendor/linux/x64
-unzip thrust-v0.7.0-linux-x64.zip
-rm thrust-v0.7.0-linux-x64.zip
+wget https://github.com/breach/thrust/releases/download/v0.7.0/thrust-v0.7.0-darwin-x64.zip
+mv thrust-v0.7.0-darwin-x64.zip ./vendor/darwin/x64
+cd ./vendor/darwin/x64
+unzip thrust-v0.7.0-darwin-x64.zip
+rm thrust-v0.7.0-darwin-x64.zip
--- release/tools/bootstrap_linux.sh
+++ release/tools/bootstrap_linux.sh
@@ -1,4 +1,3 @@
-rm -rf thrust-v*
 wget https://github.com/breach/thrust/releases/download/v0.7.0/thrust-v0.7.0-linux-x64.zip
 mv thrust-v0.7.0-linux-x64.zip ./vendor/linux/x64
 cd ./vendor/linux/x64
