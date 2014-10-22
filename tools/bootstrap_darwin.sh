556afeceab1c345c7b549d45254cb12a712e1662
Michael Hernandez <michael.hernandez1988@gmail.com>
2014-10-22 17:45:58 -0400

    Removed tools directory, no longer needed

--- tools/bootstrap_darwin.sh
+++
@@ -1,6 +0,0 @@
-wget https://github.com/breach/thrust/releases/download/v$1/thrust-v$1-darwin-x64.zip
-mkdir -p $2/vendor/darwin/x64/v$1
-mv thrust-v$1-darwin-x64.zip $2/vendor/darwin/x64/v$1/thrust-v$1-darwin-x64.zip
-cd $2/vendor/darwin/x64/v$1/
-unzip thrust-v$1-darwin-x64.zip
-rm thrust-v$1-darwin-x64.zip
