1ee1cd7ef1dd14bc7696949b3a515b00e4f629cb
Michael Hernandez <michael.hernandez1988@gmail.com>
2014-11-13 12:43:56 -0500

    Another attempt

--- spawn/helper_windows.go
+++ spawn/helper_windows.go
@@ -12,7 +12,7 @@ GetThrustDirectory returns the Directory where the unzipped thrust contents are.
 Differs between builds based on OS
 */
 func GetThrustDirectory() string {
-	return base + "\\vendor\\linux\\x64\\v" + common.THRUST_VERSION
+	return base + "\\vendor\\windows\\ia32\\v" + common.THRUST_VERSION
 }
 
 /*
@@ -20,7 +20,7 @@ GetExecutablePath returns the path to the Thrust Executable
 Differs between builds based on OS
 */
 func GetExecutablePath() string {
-	return GetThrustDirectory() + "\\thrust_shell"
+	return GetThrustDirectory() + "\\thrust_shell.exe"
 }
 
 /*
@@ -28,7 +28,7 @@ GetDownloadUrl returns the interpolatable version of the Thrust download url
 Differs between builds based on OS
 */
 func GetDownloadUrl() string {
-	return "https://github.com/breach/thrust/releases/download/v$V/thrust-v$V-linux-x64.zip"
+	return "https://github.com/breach/thrust/releases/download/v$V/thrust-v$V-windows-ia32.zip"
 }
 
 /*
