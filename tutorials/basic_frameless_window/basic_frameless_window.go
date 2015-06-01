812bbb452a8b98afb0f3f2b97d632eb0e9e45945
oda <oda@headjapan.com>
2015-06-01 18:35:42 +0900

    Add frameless window tutorial

---
+++ tutorials/basic_frameless_window/basic_frameless_window.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"time"
+
+	"github.com/miketheprogrammer/go-thrust/thrust"
+	"github.com/miketheprogrammer/go-thrust/tutorials/provisioner"
+)
+
+func main() {
+
+	thrust.InitLogger()
+	// Set any Custom Provisioners before Start
+	thrust.SetProvisioner(tutorial.NewTutorialProvisioner())
+	// thrust.Start() must always come before any bindings are created.
+	thrust.Start()
+
+	thrustWindow := thrust.NewWindow(thrust.WindowOptions{
+		RootUrl:  "http://breach.cc/",
+		HasFrame: true,
+	})
+	thrustWindow.Show()
+	thrustWindow.Focus()
+
+	// Lets do a window timeout
+	go func() {
+		<-time.After(time.Second * 5)
+		thrustWindow.Close()
+		thrust.Exit()
+	}()
+
+	// In lieu of something like an http server, we need to lock this thread
+	// in order to keep it open, and keep the process running.
+	// Dont worry we use runtime.Gosched :)
+	thrust.LockThread()
+}
